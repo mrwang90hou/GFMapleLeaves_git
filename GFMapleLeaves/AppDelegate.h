89a58d7bf557d9b01336b53acc756f480200afd4
mrwang90hou <mrwang90hou@163.com>
2018-09-08 11:41:54 +0800

    初始化项目 GFMapleLeaves

---
+++ GFMapleLeaves/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GFMapleLeaves
+//
+//  Created by L on 2018/9/8.
+//  Copyright © 2018年 mrwang90hou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GFMapleLeaves/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GFMapleLeaves
+//
+//  Created by L on 2018/9/8.
+//  Copyright © 2018年 mrwang90hou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

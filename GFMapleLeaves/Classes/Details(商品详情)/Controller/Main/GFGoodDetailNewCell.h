8922e56c891e6a072e0c3d8b658065b937d9f59a
mrwang90hou <mrwang90hou@163.com>
2018-10-31 22:23:55 +0800

    GFGoodDetailNewCell【详情页】的新设计方案

---
+++ GFMapleLeaves/Classes/Details(商品详情)/Controller/Main/GFGoodDetailNewCell.h
@@ -0,0 +1,18 @@
+//
+//  GFGoodDetailNewCell.h
+//  GFMapleLeaves
+//
+//  Created by mrwang90hou on 2019/9/31.
+//  Copyright © 2019年 mrwang90hou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GFGoodDetailNewCell : UICollectionViewCell
+
+
+/* 图片 */
+
+@property (strong, nonatomic) IBOutlet UIImageView *pciIamgeView;
+
+@end

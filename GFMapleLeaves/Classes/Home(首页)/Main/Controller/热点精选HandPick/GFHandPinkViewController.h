fe241bf94be072451e7ddfbf6f265314702f14b4
mrwang90hou <mrwang90hou@163.com>
2018-11-05 02:58:38 +0800

    【今日关注、视频、显示抢购、超级划算、9块9包邮】五个collectionViewSelect统一至GFHandPinkViewController内判断获取，且目前已经完成下拉刷新功能。【排序】等功能待完善。

---
+++ GFMapleLeaves/Classes/Home(首页)/Main/Controller/热点精选HandPick/GFHandPinkViewController.h
@@ -0,0 +1,18 @@
+//
+//  GFHandPinkViewController.h
+//  GFMapleLeaves
+//
+//  Created by mrwang90hou on 2018/11/4.
+//  Copyright © 2018年 mrwang90hou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GFBaseSetViewController.h"
+@interface GFHandPinkViewController : GFBaseSetViewController
+
+/* plist数据 */
+@property (strong , nonatomic)NSString *goodPlisName;
+
+@property (nonatomic,assign) NSInteger typeNumber;
+
+@end

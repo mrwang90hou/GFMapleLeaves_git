dafaa05e7f01a72c59f9c9f8319f1372880dc016
mrwang90hou <mrwang90hou@163.com>
2018-11-06 00:19:28 +0800

    【GFHandPink】的筛选切换，进行数据请求，实现完成。修复【GFConditionFilterView】中的bug有：获取_conditionFilterView在UIWindow的Y值坐标、设计_topView用以提防用户点击返回按钮。

--- GFMapleLeaves/Classes/Home(首页)/Main/Controller/热点精选HandPick/GFHandPinkViewController.m
+++ GFMapleLeaves/Classes/Home(首页)/Main/Controller/热点精选HandPick/GFHandPinkViewController.m
@@ -28,11 +28,23 @@
 #import <MJExtension.h>
 #import "XWDrawerAnimator.h"
 #import "UIViewController+XWTransition.h"
+
+//GFConditionFilterView实现筛选窗体
+#import "GFConditionFilterView.h"
+#import "UIView+Extension.h"
 // Categories
 
 // Others
 
 @interface GFHandPinkViewController ()<UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout>
+{
+    // *存储* 网络请求url中的筛选项 数据来源：View中_dataSource1或者一开始手动的初值
+    NSArray *_selectedDataSource1Ary;
+    NSArray *_selectedDataSource2Ary;
+    NSArray *_selectedDataSource3Ary;
+    
+    GFConditionFilterView *_conditionFilterView;
+}
 /* scrollerVew */
 @property (strong , nonatomic)UICollectionView *collectionView;
 /* 搜索 */
@@ -44,6 +56,7 @@
 /* 自定义头部View */
 @property (strong , nonatomic)DCCustionHeadView *custionHeadView;
 @property (nonatomic, assign) NSInteger page;                          /**<页码*/
+@property (nonatomic, assign) NSInteger flag;                          /**<排序类型*/
 /* 具体商品数据源 */
 @property (strong , nonatomic)NSMutableArray<DCRecommendItem *> *setItem;
 @property (strong , nonatomic)NSMutableArray<DCRecommendItem2 *> *goodsItems;
@@ -65,6 +78,8 @@
 static CGFloat _lastContentOffset;
 
 static NSString *const DCCustionHeadViewID = @"DCCustionHeadView";
+static NSString *const GFConditionFilterViewID = @"GFConditionFilterView";
+
 static NSString *const DCSwitchGridCellID = @"DCSwitchGridCell";
 static NSString *const DCListGridCellID = @"DCListGridCell";
 
@@ -88,12 +103,15 @@ static NSString *const DCListGridCellID = @"DCListGridCell";
         _collectionView.dataSource = self;
         
         [_collectionView registerClass:[DCCustionHeadView class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:DCCustionHeadViewID]; //头部View
+        [_collectionView registerClass:[GFConditionFilterView class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:GFConditionFilterViewID]; //头部View
         [_collectionView registerClass:[DCSwitchGridCell class] forCellWithReuseIdentifier:DCSwitchGridCellID];//cell
         [_collectionView registerClass:[DCListGridCell class] forCellWithReuseIdentifier:DCListGridCellID];//cell
         [self.view addSubview:_collectionView];
         WEAKSELF;
         _collectionView.mj_header = [MJRefreshNormalHeader headerWithRefreshingBlock:^{
             weakSelf.page = 1;
+//            [self.goodsItems removeAllObjects];
+            [self clearData];
             [weakSelf requestData:weakSelf.typeNumber];
         }];
         
@@ -125,6 +143,8 @@ static NSString *const DCListGridCellID = @"DCListGridCell";
     
     [self setUpColl];
     
+    [self setUpConditionFilterView];
+    
     [self setUpData];
     
     [self setUpSuspendView];
@@ -140,6 +160,102 @@ static NSString *const DCListGridCellID = @"DCListGridCell";
     self.view.backgroundColor = DCBGColor;
     self.collectionView.backgroundColor = self.view.backgroundColor;
     [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleDefault;
+}
+
+-(void)setUpConditionFilterView{
+    //方案二
+    // FilterBlock 选择下拉菜单选项触发
+    //        GFConditionFilterView *headerView= [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:GFConditionFilterViewID forIndexPath:indexPath];
+    //        _conditionFilterView =
+    _conditionFilterView = [GFConditionFilterView conditionFilterViewWithFilterBlock:^(BOOL isFilter, NSArray *dataSource1Ary, NSArray *dataSource2Ary, NSArray *dataSource3Ary) {
+        
+        // 1.isFilter = YES 代表是用户下拉选择了某一项
+        // 2.dataSource1Ary 选择后第一组选择的数据  2 3一次类推
+        // 3.如果你的项目没有清空筛选条件的功能，可以无视else 我们的app有清空之前的条件，重置，所以才有else的逻辑
+        
+        if (isFilter) {
+            //网络加载请求 存储请求参数
+            _selectedDataSource1Ary = dataSource1Ary;
+            _selectedDataSource2Ary = dataSource2Ary;
+            _selectedDataSource3Ary = dataSource3Ary;
+            
+        }else{
+            // 不是筛选，全部赋初值（在这个工程其实是没用的，因为tableView是选中后必选的，即一旦选中就没有空的情况，但是如果可以清空筛选条件的时候就有必要 *重新* reset data）
+            _selectedDataSource1Ary = @[@"综合排序"];
+            _selectedDataSource2Ary = @[];
+            _selectedDataSource3Ary = @[];
+            NSLog(@"isFilter = NO !综合排序!");
+            //记录当前请求的type 数值
+        }
+        //判断当前的状态选择，并进行网络请求
+        [self startRequest];
+    }];
+    _conditionFilterView.y += 64;
+    // 设置初次加载显示的默认数据 即初次加载还没有选择操作之前要显示的标题数据
+    //    _selectedDataSource1Ary = @[@"综合排序"];
+    //    _selectedDataSource1Ary = @[@"优惠券"];
+    //    _selectedDataSource2Ary = @[@"券后价"];
+    //    _selectedDataSource3Ary = @[@"销量"];
+    // 传入数据源，对应三个tableView顺序
+    //    _conditionFilterView.dataAry1 = @[@"1-1",@"1-2",@"1-3",@"1-4",@"1-5"];
+    //    _conditionFilterView.dataAry2 = @[@"2-1",@"2-2",@"2-3",@"2-4",@"2-5"];
+    //    _conditionFilterView.dataAry3 = @[@"3-1",@"3-2",@"3-3",@"3-4",@"3-5"];
+    _conditionFilterView.dataAry1 = @[@"综合排序",@"优惠券从高到低",@"优惠券从低到高"];
+    _conditionFilterView.dataAry2 = @[@"券后价由高到低",@"券后价由低到高"];
+    _conditionFilterView.dataAry3 = @[@"销量由高到低",@"销量由低到高"];
+    
+    // 初次设置默认显示数据(标题)，内部会调用block 进行第一次数据加载
+    [_conditionFilterView bindChoseArrayDataSource1:_selectedDataSource1Ary DataSource2:_selectedDataSource2Ary DataSource3:_selectedDataSource3Ary];
+    /** 外部手动筛选加载*/
+    [_conditionFilterView choseSortFromOutsideWithFirstSort:_selectedDataSource1Ary WithSecondSort:_selectedDataSource2Ary WithThirdSort:_selectedDataSource3Ary];
+//    [self.view addSubview:_conditionFilterView];
+}
+
+- (void)startRequest
+{
+    NSString *source1 = [NSString stringWithFormat:@"%@",_selectedDataSource1Ary.firstObject];
+    NSString *source2 = [NSString stringWithFormat:@"%@",_selectedDataSource2Ary.firstObject];
+    NSString *source3 = [NSString stringWithFormat:@"%@",_selectedDataSource3Ary.firstObject];
+    
+    //    NSMutableArray *nsArr = [NSMutableArray new];
+    //    [nsArr addObject:source1];
+    //    [nsArr addObject:source2];
+    //    [nsArr addObject:source3];
+    NSArray *strArr = @[source1,source2,source3];
+    //    NSArray *strArr = [nsArr copy];
+    NSString *typeStr = @"";
+    for (NSString *str in strArr) {
+        //        if (str.length != 0) {
+        //        if (![str isEqual:[NSNull null]]) {
+        //        if(![str isKindOfClass:[NSNull class]] && ![str isEqual:[NSNull null]] && str != nil){
+        if (![str isEqualToString:@"(null)"]) {
+            typeStr = str;
+            break;
+        }
+    }
+    NSLog(@"typeStr = %@",typeStr);
+    int flagNumer = 0;
+    NSArray *typeStrArr = @[@"综合排序",@"优惠券从高到低",@"优惠券从低到高",@"券后价由高到低",@"券后价由低到高",@"销量由高到低",@"销量由低到高"];
+    for (int i = 0; i<typeStrArr.count; i++) {
+        if ([typeStr isEqualToString:[typeStrArr objectAtIndex:i]]) {
+            flagNumer = i;
+            break;
+        }
+    }
+    NSLog(@"当前查询方法为：%@,第%d种查询方法！！！",[typeStrArr objectAtIndex:flagNumer],flagNumer);
+    //flag=0综合查询,1优惠券面值高到低，2优惠券面值低到高，3券后价由高到低，4、券后价由低到高，5，销量由高到低，6，销量由低到高
+    //    NSDictionary *dic = [_conditionFilterView keyValueDic];
+    // 可以用字符串在dic换成对应英文key
+    NSLog(@"\n第一个条件:%@\n  第二个条件:%@\n  第三个条件:%@\n",source1,source2,source3);
+    if (self.flag == flagNumer) {
+        return;
+    }
+    self.flag = flagNumer;
+    [self ScrollToTop];//滚回页面顶部
+    //清空数据
+    [self.goodsItems removeAllObjects];
+    [self.collectionView reloadData];
+    [self requestData:self.typeNumber];
 }
 
 #pragma mark - 加载数据
@@ -161,7 +277,7 @@ static NSString *const DCListGridCellID = @"DCListGridCell";
     NSDictionary *dict=@{
                          //flag=0综合查询,1优惠券面值高到低，2优惠券面值低到高，3券后价由高到低，4、券后价由低到高，5，销量由高到低，6，销量由低到高
                          @"page":@(self.page),
-                         @"flag":@"0"
+                         @"flag":@(self.flag)
                          };
     [GCHttpDataTool getHandPinkWithDict:dict typeNumber:type success:^(id responseObject) {
         [SVProgressHUD dismiss];
@@ -182,7 +298,8 @@ static NSString *const DCListGridCellID = @"DCListGridCell";
     NSDictionary *dict=@{
                          //flag=0综合查询,1优惠券面值高到低，2优惠券面值低到高，3券后价由高到低，4、券后价由低到高，5，销量由高到低，6，销量由低到高
                          @"page":@(self.page),
-                         @"flag":@"0"
+//                         @"flag":@"0",
+                         @"flag":@(self.flag)
                          };
     [GCHttpDataTool getHandPinkWithDict:dict typeNumber:type success:^(id responseObject) {
         [SVProgressHUD dismiss];
@@ -318,21 +435,29 @@ static NSString *const DCListGridCellID = @"DCListGridCell";
     
     UICollectionReusableView *reusableview = nil;
     if (kind == UICollectionElementKindSectionHeader){
-        
         DCCustionHeadView *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:DCCustionHeadViewID forIndexPath:indexPath];
         WEAKSELF
         headerView.filtrateClickBlock = ^{//点击了筛选
             [weakSelf filtrateButtonClick];
         };
-        headerView.changeViewClickBlock = ^{
-//            [weakSelf filtrateButtonClick];
+//        headerView.changeViewClickBlock = ^{
+////            [weakSelf filtrateButtonClick];
+//            [weakSelf switchViewButtonBarItemClick:self.switchViewButton];
+//        };
+//        [headerView setHidden:YES];
+        reusableview = headerView;
+        
+        [headerView addSubview:_conditionFilterView];
+        [_conditionFilterView  mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.edges.equalTo(headerView);
+        }];
+        _conditionFilterView.changeViewClickBlock = ^{
+            //            [weakSelf filtrateButtonClick];
             [weakSelf switchViewButtonBarItemClick:self.switchViewButton];
         };
-        reusableview = headerView;
     }
     return reusableview;
 }
-
 #pragma mark - item宽高
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
     return (_isSwitchGrid) ? CGSizeMake(ScreenW, 140) : CGSizeMake((ScreenW - 4)/2, (ScreenW - 4)/2 + 60);//列表、网格Cell
@@ -341,6 +466,7 @@ static NSString *const DCListGridCellID = @"DCListGridCell";
 #pragma mark - head宽高
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout referenceSizeForHeaderInSection:(NSInteger)section {
     return CGSizeMake(ScreenW, 40); //头部
+//    return CGSizeMake(ScreenW, 0);
 }
 
 #pragma mark - 边间距属性默认为0
